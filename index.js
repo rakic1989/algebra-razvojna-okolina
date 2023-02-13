8d9769800ee4c356aef84870073a92f5ec2681a7
Marko Rakić <rakic1989@gmail.com>
2023-02-13 18:53:13 +0100

    Show date in HTML

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+/**
+ * Napišite JavaScript kod koji će u HTML element s ID "date"upisati trenutni datum i vrijeme.
+ */
+
 console.log("Hello world!");
+
+const dateElement = document.querySelector("#date");
+const today = new Date();
+
+dateElement.innerHTML = today;
